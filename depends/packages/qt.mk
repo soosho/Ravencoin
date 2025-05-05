409cf4a5120bffbd9ba804478acdb6d587597b72
Soosho <162333149+soosho@users.noreply.github.com>
2025-05-05 23:56:04 +0700

    Update qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -1,21 +1,22 @@
 package=qt
 $(package)_version=5.12.11
 $(package)_download_path=https://download.qt.io/archive/qt/5.12/5.12.11/submodules/
-$(package)_file_name=qtbase-everywhere-src-5.12.11.tar.xz
+$(package)_file_name=qtbase-everywhere-src-$($(package)_version).tar.xz
 $(package)_sha256_hash=1c1b4e33137ca77881074c140d54c3c9747e845a31338cfe8680f171f0bc3a39
 $(package)_dependencies=openssl
 $(package)_linux_dependencies=freetype fontconfig libxcb libxkbcommon
 $(package)_qt_libs=corelib network widgets gui plugins testlib
-$(package)_patches=fix_qt_pkgconfig.patch mac-qmake.conf fix_no_printer.patch no-xlib.patch
-$(package)_patches+=fix_android_qmake_conf.patch fix_android_jni_static.patch dont_hardcode_pwd.patch
-$(package)_patches+=drop_lrelease_dependency.patch no_sdk_version_check.patch
-$(package)_patches+=fix_lib_paths.patch fix_android_pch.patch
-$(package)_patches+=qtbase-moc-ignore-gcc-macro.patch fix-min-max-gcc11.patch
 
-$(package)_qttranslations_file_name=qttranslations-everywhere-src-5.12.11.tar.xz
+$(package)_patches=fix_qt_pkgconfig.patch mac-qmake.conf fix_no_printer.patch no-xlib.patch
+$(package)_patches+= fix_android_qmake_conf.patch fix_android_jni_static.patch dont_hardcode_pwd.patch
+$(package)_patches+= drop_lrelease_dependency.patch no_sdk_version_check.patch
+$(package)_patches+= fix_lib_paths.patch fix_android_pch.patch
+$(package)_patches+= qtbase-moc-ignore-gcc-macro.patch
+
+$(package)_qttranslations_file_name=qttranslations-everywhere-src-$($(package)_version).tar.xz
 $(package)_qttranslations_sha256_hash=577b0668a777eb2b451c61e8d026d79285371597ce9df06b6dee6c814164b7c3
 
-$(package)_qttools_file_name=qttools-everywhere-src-5.12.11.tar.xz
+$(package)_qttools_file_name=qttools-everywhere-src-$($(package)_version).tar.xz
 $(package)_qttools_sha256_hash=98b2aaca230458f65996f3534fd471d2ffd038dd58ac997c0589c06dc2385b4f
 
 $(package)_extra_sources  = $($(package)_qttranslations_file_name)
